a468933f50d74473a22285883099d0be618eb300
b1cute <nosugerinthetea@gmail.com>
2018-06-11 00:15:30 +0900

    Bad bug fixed

--- simplex/simplex.py
+++ simplex/simplex.py
@@ -2,7 +2,6 @@
 
 import sys
 import numpy as np
-from fractions import Fraction
 
 a = np.array([[1, 1, -1],
              [2, -2, 1],
@@ -10,6 +9,7 @@ a = np.array([[1, 1, -1],
              [0, -1, -1]])
 a = a.astype(float)
 
+#メイン関数
 def main(arr, PErow, PEcol):
   if answerJudge(arr) == False:
     if stopJudge(arr, PEcol) == True:
@@ -23,13 +23,13 @@ def main(arr, PErow, PEcol):
   else:
     print("最適解は")
     for i in range(len(arr)-1):
-      print("x{} = [{}]".format(i+1,arr[len(arr)-1-i][0]))
+      print("x{} = [{}]".format(i+1,arr[len(arr)-2-i][0]))
     print("Z = {}".format(arr[len(arr)-1][0]))
     sys.exit()
 
 
+#ピボットエレメントの行番号を取得
 def findPEcol(arr):
-  count = 0
   for i in range(len(arr)-1):
     if arr[len(arr)-1][i] <= 0:
       PEcol = i
@@ -42,51 +42,50 @@ def findPEcol(arr):
   return PEcol
  
 
+#ピボットエレメントの列番号を取得
 def findPErow(arr, PEcol):
   PErow = 0
   for i in range(len(arr)-2):
-    if arr[PErow][0]/arr[PErow][PEcol] > arr[i+1][0]/arr[i+1][PEcol]:
+    if arr[i+1][PEcol] > 0 and arr[PErow][0]/arr[PErow][PEcol] > arr[i+1][0]/arr[i+1][PEcol]:
       PErow = i+1
   print("PEは[{}][{}]の{}".format(PErow, PEcol, arr[PErow][PEcol]))
   return PErow
 
 
+#掃き出しの計算
 def sweepOut(arr, PErow, PEcol):
-  print(arr)
+  b = np.empty([len(arr),len(arr[0])])
+  b = b.astype(float)
   for i in range(len(arr[0])):
-    arr[PErow,i] /= arr[PErow,PEcol]
+    b[PErow,i] /= arr[PErow,PEcol]
   for i in range(len(arr)):
     if i != PErow:
       for j in range(len(arr[0])):
-        arr[i][j] = arr[i][j] - arr[PErow,j] * arr[i][PEcol]
-    
+        b[i][j] = arr[i][j] - b[PErow,j] * arr[i][PEcol]
+  for i in range(len(arr)):
+    for j in range(len(arr[0])):
+      arr[i][j] = b[i][j]
   print(arr)
-#掃き出しの計算を書く
-#Fraction使わない方がよい
 
 
+#最適解の有無の判定
 def stopJudge(arr, PEcol):
   for i in range(len(arr)):
     if arr[i][PEcol] > 0:
       return True
       break
-#  print("最適解は存在しません")
-#  sys.exit()
     else:
       return False
-#ifで回してreturn true,breakする
 
-
+#停止条件の判定
 def answerJudge(arr):
   for i in range(len(arr[0])):
     if arr[len(arr)-1][i] < 0:
       return False
       break
   return True
-#  print("最適解です")
-#  sys.exit()
 
-
+print(a)
 PEcol = findPEcol(a)
 PErow = findPErow(a,PEcol)
 main(a,PErow,PEcol)
